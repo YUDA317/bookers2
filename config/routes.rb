6cb6e1bf3823bd3dae04cff23c460cc816e98080
EC2 Default User <ec2-user@ip-172-31-19-134.ap-northeast-1.compute.internal>
2024-04-27 14:52:13 +0000

    [add]名前でログインできるように設定でエラーが発生詰まったので明日メンター質問してみる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
-  get 'homes/about'
   devise_for :users
+  root to: "homes#top"
+  get '/about' => "homes#about",as: 'about'
   resources :books, only: [:new, :index, :show, :edit]
   resources :users, only: [:show, :edit, :update]
 
