c4e3885a8523c3c5d47296b2346193e7695ba38e
EC2 Default User <ec2-user@ip-172-31-19-134.ap-northeast-1.compute.internal>
2024-04-30 13:37:17 +0000

    [add]プロフィール画像の追加途中エラーが発生修正できないため明日メンター質問する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,17 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def index
+    @books = Book.all
     @book = Book.new
     @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
